89b5883e0920fc3b6b6701b14ead9f49e9b8502f
Hooray <hooray@hush.com>
2012-10-17 00:58:03 +0200

    Use mpmessage.hxx in FGMultiplayerTraffic headers

--- src/multiplayer/traffic.hxx
+++ src/multiplayer/traffic.hxx
@@ -1,6 +1,8 @@
 #ifndef __TRAFFIC_HXX
 #define __TRAFFIC_HXX
+
  #include <simgear/structure/subsystem_mgr.hxx>
+ #include "../flightgear/mpmessages.hxx"
 
  class FGMultiplayerTraffic : public SGSubsystem {
  public:
