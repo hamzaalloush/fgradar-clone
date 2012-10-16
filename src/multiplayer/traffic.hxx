5f31ab6dd3449fc772f02b0d0481d323f41d79c2
Hooray <hooray@hush.com>
2012-10-16 22:58:15 +0200

    Add empty stubs for a new FGMultiplayer subsystem to get traffic information from fgms

---
+++ src/multiplayer/traffic.hxx
@@ -0,0 +1,11 @@
+#ifndef __TRAFFIC_HXX
+#define __TRAFFIC_HXX
+ #include <simgear/structure/subsystem_mgr.hxx>
+
+ class FGMultiplayerTraffic : public SGSubsystem {
+ public:
+ protected:
+ private:
+ };
+#endif
+
