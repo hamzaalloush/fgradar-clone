0c78608e4e814333e396a967a247a45b942d8366
icecode <fernando.garli@gmail.com>
2012-10-18 20:18:00 +0200

    Fix rebundant delete

--- src/simgear/structure/SGApplication.cxx
+++ src/simgear/structure/SGApplication.cxx
@@ -5,12 +5,12 @@
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
-   
+ *   
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
@@ -38,14 +38,12 @@ SGApplication::SGApplication(int argc, char **argv) :
 }
 
 /**
- * Free previously allocated memory (check for NULL; only free if it is NOT
- * NULL). A case where a pointer can still be NULL can occur when the user
- * interrupted the program before the constructor gets executed.
+ * Free previously allocated memory.
  */
 SGApplication::~SGApplication()
 {
-     if (!m_subsystem_mgr) delete m_subsystem_mgr;
-     if (!m_property_tree) delete m_property_tree;
+     delete m_subsystem_mgr;
+     delete m_property_tree;
 }
 
 /**
