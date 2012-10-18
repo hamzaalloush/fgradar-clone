1d8fd169ef8c00d1e64d49b3124319e1a530a1c5
Hooray <hooray@hush.com>
2012-10-18 22:14:41 +0200

    FGOSGApplication.hxx is just a stub ATM

---
+++ src/simgear/structure/FGOSGApplication.hxx
@@ -0,0 +1,12 @@
+/*
+	The Canvas subsystem will need something:
+
+	- to attach its cameras to (osg::Group).
+	
+	- also, there has to be a property tree which will be used to control the canvas and 
+	also by the canvas itself to parse informations back.
+	
+	- For user interaction, and displaying windows there will be needed an osgViewer and 
+	a GUI camera (camera with orthogonal projection whose output goes to the application window/screen.
+ */
+
