4280f5807fc92feb8b3d5f0443729a4d81abb860
icecode <fernando.garli@gmail.com>
2012-10-21 17:09:19 +0200

    Removed a test function (get_property_tree()).

--- src/simgear/structure/SGApplication.hxx
+++ src/simgear/structure/SGApplication.hxx
@@ -91,8 +91,6 @@ public:
       */
      virtual void quit();
 
-     static SGPropertyNode *getPropertyTree() {return m_property_tree;}
-     
      /**
       * Get a property tree node. Once you have the node, you can access
       * its value by calling 'get<type>Value', where in type you specify
