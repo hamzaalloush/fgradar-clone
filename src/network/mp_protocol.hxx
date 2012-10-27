95befc88e313efa4b246b30ca85b2cbe4a343950
icecode <fernando.garli@gmail.com>
2012-10-27 12:06:46 +0200

    Improved the CMakeLists. Support for SQLite (will be needed for later use). Some work in the multiplayer protocol interface (stub).

---
+++ src/network/mp_protocol.hxx
@@ -0,0 +1,37 @@
+/**
+ * \file mp_protocol.hxx
+ * \brief Header for mp_protocol.cxx.
+ */
+
+/*
+ * Copyright (C) 2012  Fernando García  <fernando.garli@gmail.com>
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+   
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ 
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#ifndef _MP_PROTOCOL_HXX_
+#define _MP_PROTOCOL_HXX_
+
+namespace fgradar {
+
+     class MPProtocol {
+     public:
+
+     private:
+          
+     };
+     
+} // namespace fgradar
+
+#endif // _MP_PROTOCOL_HXX_
